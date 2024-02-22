64135f64fc6935aa2f30f1e491036f7221c42ed9
Sabri Çakır <sabri.cakir@lab2023.com>
2024-02-22 15:54:41 +0300

    Add theme toggle functionality and Add tooltips to icons

--- application.js
+++ application.js
@@ -53,25 +53,77 @@ window.onload = function() {
   css.type = "text/css";
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
-};
 
+  this.userTheme = localStorage.getItem("theme");
+  this.systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  this.themeCheck();
+}
 
-var switchButton = document.getElementById('toggleB');
+function themeCheck() {
+  if(this.userTheme === "dark" || (!this.userTheme && this.systemTheme)) {
+    setDarkTheme();
+  }
+  else {
+    setLightTheme();
+  }
+}
 
-switchButton.addEventListener('click', function(){
+function setDarkTheme() {
+  var lightIcons = document.querySelectorAll('.light-icon');
+  var darkIcons = document.querySelectorAll('.dark-icon');
+  var lightTexts = document.querySelectorAll('.text-black');
+  var lightHeaderTexts = document.querySelectorAll('.text-black-header');
+  var switchButton = document.getElementById('toggleB');
+  for(var i=0; i<lightIcons.length; i++){
+    lightIcons[i].classList.add('hidden');
+  }
+  for(var i=0; i<darkIcons.length; i++){
+    darkIcons[i].classList.remove('hidden');
+  }
+  for(var i=0; i<lightTexts.length; i++){
+    lightTexts[i].classList.add('text-slate-100');
+  }
+  for(var i=0; i<lightHeaderTexts.length; i++){
+    lightHeaderTexts[i].classList.add('text-[#04aa6d]');
+  }
+  switchButton.checked = true;
+  document.body.classList.add('bg-slate-700');
+}
+
+function setLightTheme() {
   var lightIcons = document.querySelectorAll('.light-icon');
   var darkIcons = document.querySelectorAll('.dark-icon');
   var lightTexts = document.querySelectorAll('.text-black');
   var lightHeaderTexts = document.querySelectorAll('.text-black-header');
   for(var i=0; i<lightIcons.length; i++){
-    lightIcons[i].classList.toggle('hidden');
-    darkIcons[i].classList.toggle('hidden');
+    lightIcons[i].classList.remove('hidden');
+  }
+  for(var i=0; i<darkIcons.length; i++){
+    darkIcons[i].classList.add('hidden');
   }
   for(var i=0; i<lightTexts.length; i++){
-    lightTexts[i].classList.toggle('text-slate-100');
+    lightTexts[i].classList.remove('text-slate-100');
   }
   for(var i=0; i<lightHeaderTexts.length; i++){
-    lightHeaderTexts[i].classList.toggle('text-[#04aa6d]');
+    lightHeaderTexts[i].classList.remove('text-[#04aa6d]');
   }
-  document.body.classList.toggle('bg-slate-700');
-})
+  switchButton.checked = false;
+  document.body.classList.remove('bg-slate-700');
+}
+
+function toggleTheme() {
+  if(this.userTheme === "dark") {
+    localStorage.setItem("theme", "light");
+    this.userTheme = "light";
+    setLightTheme();
+  }
+  else {
+    localStorage.setItem("theme", "dark");
+    this.userTheme = "dark";
+    setDarkTheme();
+  }
+
+}
+
+var switchButton = document.getElementById('toggleB');
+switchButton.addEventListener('click', toggleTheme);
